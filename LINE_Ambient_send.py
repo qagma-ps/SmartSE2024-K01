f8f5152ecb47d4ce9c062e3a4bea9a833d8ffe00
Yuichiro Abe <yuichiro.a30@gmail.com>
2024-07-06 07:59:18 +0900

    20240706

--- LINE_Ambient_send.py
+++ LINE_Ambient_send.py
@@ -6,18 +6,15 @@
 # 気温は英語でtemperatureですが、「℃(度)」と具体的に温度を表現したいときは「degree」を使います。
 # 複数形の"s"も忘れないようにつけましょう。
 
-# Ambientチャネル名「k01_team5_チャネル」
+# Ambientチャネル
 channelId = 12345
-writeKey = XXXX
+channelId_geo = 12345
+writeKey = "XXX"
+writeKey_geo = "XXXX"
 
 # LINE
-token = XXXX
-message = "厳重警戒レベル(暑さ指数WBGT値=30℃)"
-
-# 温度、湿度、暑さ指数(WBGT)
-temp = 30.0
-humid = 70.0
-wbgt = 27
+token = "XXXXX"
+msg = "厳重警戒レベル(暑さ指数WBGT値=30℃)"
 
 # ■モジュールのインストールコマンド
 # pi$ sudo pip install git+https://github.com/AmbientDataInc/ambient-python-lib.git
@@ -25,23 +22,81 @@ wbgt = 27
 # pi$ pip freeze | grep ambient
 # ambient==0.1.0
 
+import csv
+
 # Ambient送信関数
 import ambient
 
 
-def send_ambient(channelId, writeKey, temp, humid):
+def send_ambient(channelId, writeKey, data):
     ambi = ambient.Ambient(channelId, writeKey)
-    r = ambi.send({"d1": temp, "d2": humid})
+    r = ambi.send(data)
+
+
+def convert_format_ambient(file_path):
+    data = []
+    with open(file_path, mode="rb") as file:
+        file_content = file.read().replace(b"\x00", b"")  # Remove null bytes
+        file_content = file_content.decode("utf-8").splitlines()
+
+        csv_reader = csv.DictReader(file_content)
+
+        for row in csv_reader:
+            # Handle potential missing or invalid data
+            try:
+                created = row["Timestamp"]
+                d1 = float(row["Temperature"])
+                d2 = float(row["Humidity"])
+                data.append({"created": created, "d1": d1, "d2": d2})
+            except (ValueError, TypeError):
+                # Skip rows with invalid data
+                continue
+    return data
+
+
+def convert_geo_format_ambient(file_path_geo):
+    data_geo = []
+    with open(file_path_geo, mode="rb") as file:
+        file_content = file.read().replace(b"\x00", b"")  # Remove null bytes
+        file_content = file_content.decode("utf-8").splitlines()
+
+        csv_reader = csv.DictReader(file_content)
+
+        for row in csv_reader:
+            # Handle potential missing or invalid data
+            try:
+                created = row["Timestamp"]
+                lat = float(row["Latitude"])
+                lng = float(row["Longitude"])
+                data_geo.append(
+                    {"created": created, "d1": 0, "d2": 0, "lat": lat, "lng": lng}
+                )
+            except (ValueError, TypeError):
+                # Skip rows with invalid data
+                continue
+    return data_geo
 
 
 # LINEメッセージ送信関数
 import requests
 
 
-def send_line_message(token, message):
+def send_line_message(msg):
     url = "https://notify-api.line.me/api/notify"
-    token = token
-    headers = {"Authorization": "Bearer" + token}
-    message = message
+    headers = {"Authorization": "Bearer " + token}
+    message = msg
     payload = {"message": message}
     r = requests.post(url, headers=headers, params=payload)
+    print(r)
+
+
+# usage
+if __name__ == "__main__":
+    # file_path = '/home/pi/Documents/20240629K01/temp_data.csv'
+    file_path_geo = "/home/pi/Documents/20240629K01/geo_data.csv"
+    # converted_data = convert_format_ambient(file_path)
+    converted_data_geo = convert_geo_format_ambient(file_path_geo)
+    print(converted_data_geo[0])
+    # send_ambient(channelId, writeKey, converted_data)
+    send_ambient(channelId_geo, writeKey_geo, converted_data_geo)
+# send_line_message(msg)
